e4b06b8e263841b6dd052440acbbefb3590416c6
ramyaakuthota19 <ramyaakuthota19@gmail.com>
2025-11-09 16:19:57 +0530

    Add project files.

---
+++ dot net3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_net3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
